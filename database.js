cf6b10649a7d5a21363fe76a587dc64e2e86704c
ahmadkharissa <ahmadharissa25@gmail.com>
2023-06-12 12:15:28 -0700

    format

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const db = mongoose.connect(process.env.DB_CONNECTION)
-    .then(res => console.log("connection success"))
-    .catch(e => console.log(e));
+const db = mongoose
+  .connect(process.env.DB_CONNECTION)
+  .then((res) => console.log("connection success"))
+  .catch((e) => console.log(e));
 
-export default db
+export default db;
